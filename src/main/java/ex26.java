a843f54fcf3c9c6fbbf1b888efc7660e9b35e727
Brandon Barbour <brandon8barbour@gmail.com>
2021-06-13 23:14:19 -0400

    Initial commit

---
+++ src/main/java/ex26.java
@@ -0,0 +1,34 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 26 Solution
+ *  Copyright 2021 Brandon Barbour
+ */
+
+import java.util.Scanner;
+
+public class ex26 {
+    public static void main(String[] args) {
+        System.out.print("What is your balance? ");
+        Scanner input = new Scanner(System.in);
+        double bal = input.nextDouble();
+        System.out.print("What is the APR on the card (as a percent)? ");
+        double apr100 = input.nextDouble();
+        double apr = apr100 / 100 / 365;
+        System.out.print("What is the monthly payment you can make? ");
+        double mpay = input.nextDouble();
+
+        double n = PaymentCalculator.calculateMonthsUntilPaidOff(bal, apr, mpay);
+
+        System.out.printf("It will take you %.0f months to pay off this card.", n);
+
+    }
+}
+
+class PaymentCalculator {
+    public static double calculateMonthsUntilPaidOff(double bal, double apr, double mpay) {
+        double n = -(1.000000 / 30.000000) * (java.lang.Math.log(1 + (bal / mpay) * (1 - java.lang.Math.pow((1 + apr), 30)))) / java.lang.Math.log(1 + apr);
+        if((n % 1) != 0) { //round up months
+            n++;
+        }
+        return n;
+    }
+}
